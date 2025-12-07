956bb8a833e7293fcb8fa961c8bd0e4ba4dfa07e
Mariano Ravinale <mravinale@gmail.com>
2025-12-07 19:23:48 -0300

    moved to potgres

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,19 @@
+import 'dotenv/config';
 import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
+import { Pool } from "pg";
 
 export const auth = betterAuth({
-    database: new Database("sqlite.db"),
-    baseURL: "http://localhost:3000",
+    database: new Pool({
+        connectionString: process.env.DATABASE_URL || "postgresql://mravinale@localhost:5432/nestjs-api-starter",
+    }),
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
     basePath: "/api/auth",
-    trustedOrigins: ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+    trustedOrigins: process.env.TRUSTED_ORIGINS?.split(",") || [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+    ],
     emailAndPassword: {
         enabled: true,
     },
